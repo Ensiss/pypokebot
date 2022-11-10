491edd5cf290b403197b1e7ec715cb9c5fe904c2
Ensis <7736729+Ensiss@users.noreply.github.com>
2022-11-10 22:55:13 +0100

    Switch opencv->sdl, add proper game loop and key input events

--- main.py
+++ main.py
@@ -5,40 +5,79 @@ import os
 import re
 
 import numpy as np
-import cv2
 import mgba.core
 import mgba.image
 import mgba.log
+import pygame
 
-import memory as mem
 import world
+import pokedata
+import utils
+import memory; mem = memory.Memory
+import database; db = database.Database
 
 parser = argparse.ArgumentParser(description="Pokebot")
 parser.add_argument("-r", "--rom", type=str, default=os.path.expanduser("~/Games/Pokemon - FireRed Version (USA).gba"),
                     help="Path to the Pokemon Firered v1.0 ROM")
 
 args = parser.parse_args()
-
 mgba.log.silence()
 core = mgba.core.load_path(args.rom)
 core.autoload_save()
-screen = mgba.image.Image(*core.desired_video_dimensions())
-core.set_video_buffer(screen)
+size = core.desired_video_dimensions()
+screen_buf = mgba.image.Image(*size)
+core.set_video_buffer(screen_buf)
 core.reset()
 mem.init(core)
+db.init()
 
-for i in range(20000):
-    core.clear_keys(core.KEY_A)
-    if i % 2 == 0:
-        core.add_keys(core.KEY_A)
-    core.run_frame()
-    mem.updateBuffers()
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+keymap = [(pygame.K_UP, core.KEY_UP),
+          (pygame.K_DOWN, core.KEY_DOWN),
+          (pygame.K_LEFT, core.KEY_LEFT),
+          (pygame.K_RIGHT, core.KEY_RIGHT),
+          (pygame.K_w, core.KEY_A),
+          (pygame.K_x, core.KEY_B),
+          (pygame.K_q, core.KEY_L),
+          (pygame.K_s, core.KEY_R),
+          (pygame.K_RETURN, core.KEY_START),
+          (pygame.K_BACKSPACE, core.KEY_SELECT)]
 
-    RGB = np.array(screen.to_pil())[:,:,[2,1,0]]
-    cv2.imshow("out", RGB)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
+def runGame(onPreFrame=None, nframes=-1):
+    i = 0
+    while True:
+        clock.tick(60)
+        if nframes > 0 and i >= nframes:
+            return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                return
+            elif event.type == pygame.KEYDOWN:
+                for key in keymap:
+                    if event.key == key[0]:
+                        core.add_keys(key[1])
+                        break
+            elif event.type == pygame.KEYUP:
+                for key in keymap:
+                    if event.key == key[0]:
+                        core.clear_keys(key[1])
+                        break
 
-cv2.destroyAllWindows()
+        if onPreFrame is not None:
+            onPreFrame()
+        core.run_frame()
+        mem.updateBuffers()
 
+        surface = pygame.image.frombuffer(screen_buf.to_pil().tobytes(), size, "RGBX")
+        screen.blit(surface, (0, 0))
+        pygame.display.flip()
+        i += 1
+
+def introSkipper():
+    core.clear_keys(core.KEY_B, core.KEY_A)
+    if core.frame_counter % 2 == 0:
+        core.set_keys(core.KEY_A)
+
+runGame(introSkipper, 800)
+runGame()
