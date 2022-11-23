9335e00e8395316c07324e20654f345845dd0064
Ensis <7736729+Ensiss@users.noreply.github.com>
2022-11-23 18:11:39 +0100

    Move Pathfinding logic to separate file

---
+++ core/pathfinder.py
@@ -0,0 +1,121 @@
+import numpy as np
+import database; db = database.Database
+
+class Pathfinder:
+    mvt_static = [0, 1, 7, 8, 9, 10] + list(range(13, 25)) + list(range(64, 80))
+
+    class Node:
+        def __init__(self, m, coords):
+            self.map = m
+            self.x, self.y = coords
+            self.status = m.map_status[self.y, self.x]
+            self.tile = m.map_tile[self.y, self.x]
+            self.behavior = m.map_behavior[self.y, self.x]
+            self.bg = m.map_bg[self.y, self.x]
+            self.left = self.right = None
+            self.up = self.down = None
+            self.g = self.f = 0
+
+        def isWalkable(self):
+            return (self.status in [0x0C, 0x00, 0x10] and   # Walkable tile
+                    self.behavior not in [0x61, 0x6B])      # Not escalator
+
+        def hasOverWorld(self):
+            for pers in self.map.persons:
+                if (pers.x == self.x and pers.y == self.y and pers.isVisible() and
+                    pers.mvt_type in Pathfinder.mvt_static):
+                    return True
+            for ow in db.ows[1:]: # Skip player overworld
+                if ow.map_id == 0 and ow.bank_id == 0:
+                    break
+                if (ow.bank_id == db.player.bank_id and
+                    ow.map_id == db.player.map_id and
+                    ow.dest_x == self.x and ow.dest_y == self.y):
+                    return True
+            return False
+
+        def connect(self, pth):
+            right = pth.getNode(self.x+1, self.y)
+            if right:
+                bhv = right.behavior
+                if bhv in [0x38, 0x39]: # Hills
+                    right = pth.getNode(self.x+2, self.y)
+                    if bhv == 0x38: # Right jump
+                        self.right = right
+                    elif right: # Left jump
+                        right.left = self
+                elif right.isWalkable():
+                    self.right = right
+                    right.left = self
+
+            down = pth.getNode(self.x, self.y+1)
+            if down:
+                if down.behavior == 0x3B: # Down jump
+                    self.down = pth.getNode(self.x, self.y+2)
+                elif down.isWalkable():
+                    self.down = down
+                    down.up = self
+
+        def disconnect(self):
+            if self.left:
+                self.left.right = None
+            if self.right:
+                self.right.left = None
+            if self.up:
+                self.up.down = None
+            if self.down:
+                self.down.up = None
+            self.left = self.right = None
+            self.up = self.down = None
+
+    def __init__(self, map_data):
+        self.map = map_data
+        self.nodes = []
+        # Create nodes
+        for y in range(self.map.height):
+            rows = []
+            for x in range(self.map.width):
+                n = Pathfinder.Node(self.map, (x, y))
+                rows.append(n)
+            self.nodes.append(rows)
+        # Connect nodes
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                if not self.nodes[y][x].isWalkable():
+                    continue
+                self.nodes[y][x].connect(self)
+        # Remove unwalkable nodes
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                if not self.nodes[y][x].isWalkable():
+                    self.nodes[y][x] = None
+
+    def getNode(self, x, y):
+        if not (0 <= x < self.map.width and 0 <= y < self.map.height):
+            return None
+        return self.nodes[y][x]
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        xy = []
+        ows = []
+        for row in self.nodes:
+            for node in row:
+                if node is None:
+                    continue
+                xy.append([node.x, node.y])
+                if node.hasOverWorld():
+                    ows.append([node.x, node.y])
+                if node.right:
+                    plt.plot([node.x+.1, node.right.x-.1], [node.y+.1, node.right.y+.1])
+                if node.left:
+                    plt.plot([node.x-.1, node.left.x+.1], [node.y-.1, node.left.y-.1])
+                if node.up:
+                    plt.plot([node.x-.1, node.up.x-.1], [node.y-.1, node.up.y+.1])
+                if node.down:
+                    plt.plot([node.x+.1, node.down.x+.1], [node.y+.1, node.down.y-.1])
+        plt.scatter(*np.array(xy).T, zorder=2.5)
+        plt.scatter(*np.array(ows).T, color="red", zorder=2.5)
+        plt.gca().invert_yaxis()
+        plt.gca().set_aspect(1)
+        plt.show()
