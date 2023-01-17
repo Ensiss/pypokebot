668b68fbe822e546a75ca0467febbe7ccac2bc7b
Ensis <7736729+Ensiss@users.noreply.github.com>
2023-01-17 18:38:39 +0100

    Add coin/money data to the player

--- core/player.py
+++ core/player.py
@@ -3,16 +3,19 @@ import utils
 
 class Player(utils.AutoUpdater):
     def update(self):
-        map_offset = mem.readU32(0x3005008)
-        data_offset = mem.readU32(0x300500C)
-        self.valid = (map_offset != 0 and data_offset != 0)
+        saveblock1_offset = mem.readU32(0x3005008)
+        saveblock2_offset = mem.readU32(0x300500C)
+        self.valid = (saveblock1_offset != 0 and saveblock2_offset != 0)
         (self.x,
          self.y,
          self.bank_id,
-         self.map_id) = mem.unpack(map_offset, "2H2B")
+         self.map_id) = mem.unpack(saveblock1_offset, "2H2B")
         (self.name,
          self.gender,
          self.unknown,
-         self.trainer_id) = mem.unpack(data_offset, "8S2BH")
-        self.map_offset = map_offset
-        self.data_offset = data_offset
+         self.trainer_id) = mem.unpack(saveblock2_offset, "8S2BH")
+        encryption_key = mem.readU32(saveblock2_offset + 0xF20)
+        self.money = mem.readU32(saveblock1_offset + 0x290) ^ encryption_key
+        self.coins = mem.readU16(saveblock1_offset + 0x294) ^ encryption_key
+        self.saveblock1_offset = saveblock1_offset
+        self.saveblock2_offset = saveblock2_offset
