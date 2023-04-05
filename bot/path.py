caeea4b1507195d97bce56af06786b98ef51a0da
Ensis <7736729+Ensiss@users.noreply.github.com>
2023-04-05 07:53:16 +0200

    Add full heal bot function

--- bot/path.py
+++ bot/path.py
@@ -1,6 +1,8 @@
 import memory; mem = memory.Memory
 import database; db = database.Database
 import core.io; io = core.io.IO
+from metafinder import Metafinder
+from script import Script
 import misc
 import world
 import movement
@@ -20,3 +22,16 @@ def follow(bot, path, explore=False):
         if explore:
             yield from bot.exploreMap()
         yield from func
+
+def heal(bot):
+    if (path := Metafinder.searchHealer()) is None:
+        return -1
+    yield from follow(bot, path)
+    (x, y, bid, mid), pers = path[-1]
+    pscript = Script.getPerson(pers.evt_nb-1, bid, mid)
+    choices = []
+    for ctx in pscript.execute():
+        if Script.CallSpecial(0x0) in ctx.outputs:
+            choices = ctx.choices
+            break
+    return (yield from interact.talkTo(pers, choices))
